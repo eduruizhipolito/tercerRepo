ccca145bd05c1e09efc24c610d635a682765a84f
eduruizhipolito <eduruizhipolito@gmail.com>
2025-04-07 15:41:44 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Eduardo Ruiz",                         # Tu nombre
+    author_email="edu@zajalabs.com",                 # Tu correo electrónico
+    url="https://github.com/eduruizhipolito/tercerrepb",     # URL del proyecto
+)
